5717837f174908dbde9fbc0ba3484346aff23047
babakslt <babakslt@gmail.com>
2022-09-07 07:28:44 +0430

    fixed dvars function

--- findoutlie/metrics.py
+++ findoutlie/metrics.py
@@ -2,8 +2,7 @@
 """
 
 # Any imports you need
-# +++your code here+++
-
+import numpy as np
 
 def dvars(img):
     """ Calculate dvars metric on Nibabel image `img`
@@ -21,12 +20,7 @@ def dvars(img):
         One-dimensional array with n-1 elements, where n is the number of
         volumes in `img`.
     """
-    # Hint: remember 'axis='.  For example:
-    # In [2]: arr = np.array([[2, 3, 4], [5, 6, 7]])
-    # In [3]: np.mean(arr, axis=1)
-    # Out[2]: array([3., 6.])
-    #
-    # You may be be able to solve this in four lines, without a loop.
-    # But solve it any way you can.
-    # This is a placeholder, replace it to write your solution.
-    raise NotImplementedError('Code up this function')
+    data = img.get_fdata()
+    # voxel diff  =  (data from 0 to -1) - (data from 1 to the end of array)
+    vol_diff = data[...,:-1].reshape((-1,data.shape[-1]-1))-data[...,1:].reshape((-1,data.shape[-1]-1))
+    return np.sqrt(np.mean(vol_diff**2,axis=0))
